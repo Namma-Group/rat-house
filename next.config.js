bea8ee41847ca705c95b102b125a8b1154a3fa9f
gowtham <gowthamchandrasekaran01@gmail.com>
2024-02-28 17:36:01 +0530

    nth commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: 'https://localhost:3000', // Replace with your image host URL
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
