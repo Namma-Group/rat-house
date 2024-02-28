fe340b539234236c55e3aa9948d44b73d1e3eb95
GOWTHAM C <94632496+gowtham20402@users.noreply.github.com>
2024-02-28 17:45:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'imgix',
-    path: 'https://localhost:3000', // Replace with your image host URL
-  },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
